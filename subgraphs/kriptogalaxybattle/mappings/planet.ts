adf4ba7869e83b180c531a23529c562acd54a7c8
beco-dev <>
2021-11-04 11:54:30 +0700

    add planet

---
+++ subgraphs/kriptogalaxybattle/mappings/planet.ts
@@ -0,0 +1,20 @@
+import { Created, Transfer } from "../generated/PlanetCore/PlanetCore";
+import { Planet } from "../generated/schema";
+
+export function handleCreate(args: Created): void {
+    let planet = new Planet(args.params.planetId.toString())
+    planet.cardId = args.params.cardId;
+    planet.size = args.params.size;
+    planet.rarity = args.params.size;
+    planet.owner = args.params.owner;
+    planet.save()
+}
+
+export function handleTransfer(args: Transfer): void {
+    let m = Planet.load(args.params.tokenId.toString());
+    if (m) {
+      m.owner = args.params.to;
+      m.save();
+    }
+  }
+  
