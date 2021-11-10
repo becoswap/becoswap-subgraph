fe22f0ef01f917d18c13f8a7980bb1b42cf9207e
becoswap-dev <cool@becoswap.com>
2021-11-10 19:56:59 +0700

    update

--- subgraphs/kriptogalaxybattle/mappings/planet.ts
+++ subgraphs/kriptogalaxybattle/mappings/planet.ts
@@ -2,34 +2,32 @@ import { Address, Bytes } from "@graphprotocol/graph-ts";
 import { Created, Transfer } from "../generated/PlanetCore/PlanetCore";
 import { Planet } from "../generated/schema";
 
-
-const strategies : Array<string> = [
-  "0x96534accf2d52225462cfe0a0ec18b8b37353e4d"
-]
+const strategies: Array<string> = ["0x96534accf2d52225462cfe0a0ec18b8b37353e4d"];
 
 function isStratery(addr: Address): boolean {
- return strategies.filter(v =>  v === addr.toString()).length > 0
+  return strategies.filter((v) => v === addr.toString()).length > 0;
 }
 
 export function handleCreate(args: Created): void {
-    let planet = new Planet(args.params.planetId.toString())
-    planet.cardId = args.params.cardId;
-    planet.size = args.params.size;
-    planet.rarity = args.params.rarity;
-    planet.owner = args.params.owner;
-    planet.save()
+  let planet = new Planet(args.params.planetId.toString());
+  planet.cardId = args.params.cardId;
+  planet.size = args.params.size;
+  planet.rarity = args.params.rarity;
+  planet.owner = args.params.owner;
+  planet.robots = [];
+  planet.monsters = [];
+  planet.strategy = null;
+  planet.save();
 }
 
 export function handleTransfer(args: Transfer): void {
   if (isStratery(args.params.to)) return;
-    let m = Planet.load(args.params.tokenId.toString());
-    if (m) {
-      m.owner = args.params.to;
-      m.robots = null;
-      m.monsters = null;
-      m.strategy = null;
-      m.save();
-      
-    }
+  let m = Planet.load(args.params.tokenId.toString());
+  if (m) {
+    m.owner = args.params.to;
+    m.robots = [];
+    m.monsters = [];
+    m.strategy = null;
+    m.save();
+  }
 }
-  
