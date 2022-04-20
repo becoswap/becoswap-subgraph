1fece746b577663ac5bd1b1cd997a2de03944a60
becoswap-dev <cool@becoswap.com>
2022-04-20 20:28:34 +0700

    index pvp ticket

--- subgraphs/kriptogalaxybattle/mappings/pvpTicket.ts
+++ subgraphs/kriptogalaxybattle/mappings/pvpTicket.ts
@@ -1,3 +1,14 @@
-export function handleBuy(args: Created): void {
+import { User } from "../generated/schema";
+import { Buy } from "../generated/PvPTicket/PvPTicket";
 
+export function handleBuy(args: Buy): void {
+    let userId = args.params._account.toHex();
+    let user = User.load(userId)
+    if (!user) {
+        user = new User(userId)
+        user.pvpTicket = args.params._tickets;
+    } else {
+        user.pvpTicket = user.pvpTicket.plus(args.params._tickets)
+    }
+    user.save();
 }
